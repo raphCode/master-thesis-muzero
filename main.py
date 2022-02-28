08278af6f2403d6686a35a5043394d817e6805ee
raphTec <git@raphtec.net>
2022-02-28 17:22:24 +0100

    Add draft for main training loop

---
+++ main.py
@@ -0,0 +1,14 @@
+from config import config as C
+
+state = C.game.new_initial_state()
+for _ in range(C.train.max_steps_per_episode):
+    if state.is_terminal:
+        break
+    # TODO:
+    state.current_player
+    # request action from it (mcts treesearch)
+    C.nets.representation.forward(state)
+    
+
+
+
