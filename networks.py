4369cb9d9c2adfd52484da469d14eb005d3d5227
raphTec <git@raphtec.net>
2022-03-05 21:56:45 +0100

    Add some experimental type hints to the network classes

--- networks.py
+++ networks.py
@@ -1,21 +1,30 @@
 from abc import ABC, abstractmethod
+from typing import Tuple, NewType
+
+LatentRepr = NewType("LatentRepr", torch.Tensor)
+ValueScalar = NewType("ValueScalar", torch.Tensor)
+ActionOnehot = NewType("ActionOnehot", torch.Tensor)
+RewardScalar = NewType("RewardScalar", torch.Tensor)
 
 class Network(ABC):
     model: "TODO"
 
     @abstractmethod
     def forward(self, input):
-        return self.model.forward(input)
+        pass
 
 class RepresentationNet(Network):
-    def forward(self, input: GameState) -> LatentRep:
-        pass
+    def forward(self, input: GameState) -> LatentRepr:
+        latent = self.model.forward(input)
+        return LatentRepr(latent)
 
 class PredictionNet(Network):
-    def forward(self, input: LatentRep) -> LatentRep:
-        pass
+    def forward(self, input: LatentRepr) -> Tuple[ValueScalar, ActionOnehot]:
+        value, action = self.model.forward(input)
+        return ValueScalar(value), ActionOnehot(action)
 
 class DynamicsNet(Network):
-    def forward(self, input: LatentRep) -> Action:
-        pass
+    def forward(self, input: LatentRepr) -> Tuple[RewardScalar, LatentRepr]:
+        reward, latent = self.model.forward(input)
+        return RewardScalar(reward), LatentRepr(latent)
 
