1a6eb64e0d0cb4744a08f1185a051906be5ccecc
raphTec <git@raphtec.net>
2022-05-18 13:20:11 +0200

    PlayerType: Fix typo in docstring

--- trajectory.py
+++ trajectory.py
@@ -3,7 +3,7 @@ from enum import IntEnum, auto
 
 class PlayerType(IntEnum):
     """
-    Represents whose turn it is as seen by a specific player.
+    Reprents whose turn it is as seen by a specific player.
     This is important in the recorded trajectories and in the tree search.
     The chance player models randomness in a game by taking a random action.
     """
