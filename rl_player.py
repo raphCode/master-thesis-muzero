31901a97b07df1c268f9d661413cb4177cddc1dd
raphTec <git@raphtec.net>
2022-05-18 13:19:29 +0200

    Add rl_player.py

---
+++ rl_player.py
@@ -0,0 +1,44 @@
+from collections import namedtuple
+
+import torch
+
+from mcts import run_mcts
+
+RLPResult = namedtuple(
+    "RLPResult",
+    [
+        "action",
+        "old_beliefs",
+        "target_policy",
+    ],
+)
+
+
+class RLPlayer:
+    """
+    Player which does MCTS with neural networks for move selection.
+    Don't share a single RLPlayer instance for multiple players because of the private
+    beliefs state.
+    """
+
+    beliefs: torch.Tensor
+
+    def __init__(self):
+        self.reset_new_game()
+
+    def request_action(self, obs: torch.Tensor, player_onehot: torch.Tensor) -> RLPResult:
+        """
+        Requests an action from RL agent for the current observation.
+        Returns an action to choose as well as extra data for recording a game trajectory.
+        """
+        old_beliefs = self.beliefs.to(device="cpu", copy=True)
+        latent_rep, self.beliefs = C.nets.representation.forward(
+            obs, self.beliefs, player_onehot
+        )
+        root_node = run_mcts(latent_rep, new_beliefs)
+        action = C.func.mcts_root2action(root_node)
+        target_policy = C.func.mcts_root2target_policy(root_node)
+        return RLPResult(action, old_beliefs, target_policy)
+
+    def reset_new_game(self):
+        self.beliefs = torch.zeros(tuple(C.param.belief_size))
