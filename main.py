f5d8a6151a0aa9d66af46f557873c29c8d0f936c
raphTec <git@raphtec.net>
2022-05-19 12:44:42 +0200

    main.py: Add TODO assert for non-terminal rewards

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from config import config as C
 
 state = C.game.new_initial_state()
 for _ in range(C.param.max_steps_per_episode):
+    # Unsure about how to deal with non-terminal rewards or when exactly they occur
+    assert all(r == 0 for r in state.rewards) or state.is_terminal
+
     if state.is_terminal:
         break
     # TODO:
     state.current_player
     # request action from it (mcts treesearch)
     C.nets.representation.forward(state)
-    
-
-
-
