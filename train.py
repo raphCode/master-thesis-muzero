0bd91826f1bed5ff21393e068e9b59e637c5eddd
raphTec <git@raphtec.net>
2022-05-31 23:55:12 +0200

    process_trajectory(): Correctly init latent & beliefs
    This depends on whether the first batch sample has an observation (= own
    move) or not (= latent and beliefs from simulation / tree search /
    dynamics network).

--- train.py
+++ train.py
@@ -16,8 +16,13 @@ class Loss(NamedTuple):
 def process_trajectory(traj: List[TrajectoryState], loss: Loss):
     # TODO: move tensors to GPU
 
-    beliefs = traj[0].beliefs
-    latent_rep = None
+    first = traj[0]
+    if first.observation is None:
+        beliefs = first.dyn_beliefs
+        latent_rep = first.latent_rep
+    else:
+        latent_rep, beliefs = C.nets.representation(first.observation, first.old_beliefs)
+
     for ts in traj:
         if ts.observation is not None:
             new_latent_rep, beliefs = C.nets.representation(ts.observation, beliefs)
