ccc2ac46b6c1196071519d8934be8f2085c7ac76
raphTec <git@raphtec.net>
2022-07-08 15:52:01 +0200

    networks/bases.py: Add forward() method signatures

--- networks/bases.py
+++ networks/bases.py
@@ -1,18 +1,31 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
+import torch
 import torch.nn as nn
 
 
 class RepresentationNet(nn.Module, ABC):
     # Observation, Beliefs -> LatentRep, Beliefs
-    pass
+    @abstractmethod
+    def forward(
+        self, observation: tuple[torch.Tensor, ...], beliefs: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        pass
 
 
 class PredictionNet(nn.Module, ABC):
     # LatentRep, Beliefs -> ValueScalar, Policy, PlayerType
-    pass
+    @abstractmethod
+    def forward(
+        self, latent_rep: torch.Tensor, beliefs: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        pass
 
 
 class DynamicsNet(nn.Module, ABC):
     # LatentRep, Beliefs, ActionOnehot -> LatentRep, Beliefs, RewardScalar
-    pass
+    @abstractmethod
+    def forward(
+        self, latent_rep: torch.Tensor, beliefs: torch.Tensor, action_onehot: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        pass
