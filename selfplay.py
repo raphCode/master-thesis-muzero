cd3dad4f8e41009cd0c8372f22b191cdd473c817
raphTec <git@raphtec.net>
2022-07-13 18:26:03 +0200

    selfplay.py: Put logic into run_episode() function
    This change indents all of the code so the diff looks huge.
    The actual code changes involved is that the replay buffer is supplied
    by argument, not created, whach can be seen by using --ignore-all-space
    with git diff or git show.

--- selfplay.py
+++ selfplay.py
@@ -10,98 +10,96 @@ from trajectory import PlayerType, ReplayBuffer, TrajectoryState
 
 rng = np.random.default_rng()
 
-replay_buffer = ReplayBuffer(C.train.replay_buffer_size)
 
-players = C.player.instances
-rl_pids = {n for n, p in enumerate(players) if isinstance(p, RLPlayer)}
-initial_node = Node.from_latents(C.nets.initial_latent_rep, C.nets.initial_beliefs)
+def run_episode(replay_buffer: ReplayBuffer):
+    players = C.player.instances
+    rl_pids = {n for n, p in enumerate(players) if isinstance(p, RLPlayer)}
+    initial_node = Node.from_latents(C.nets.initial_latent_rep, C.nets.initial_beliefs)
 
-mcts_nodes = {n: deepcopy(initial_node) for n in rl_pids}
+    mcts_nodes = {n: deepcopy(initial_node) for n in rl_pids}
 
+    def get_update_mcts_tree(pid: int, action: int) -> Node:
+        node = mcts_nodes[pid].get_action_subtree(action)
+        ensure_visit_count(node, C.mcts.iterations_value_estimate)
+        mcts_nodes[pid] = node
+        return node
 
-def get_update_mcts_tree(pid: int, action: int) -> Node:
-    node = mcts_nodes[pid].get_action_subtree(action)
-    ensure_visit_count(node, C.mcts.iterations_value_estimate)
-    mcts_nodes[pid] = node
-    return node
+    state = C.game.instance.new_initial_state()
+    trajectories = {n: [] for n in rl_pids}
 
+    for pid in rl_pids:
+        players[pid].reset_new_game()
 
-state = C.game.instance.new_initial_state()
-trajectories = {n: [] for n in rl_pids}
+    for _ in range(C.train.max_steps_per_episode):
+        # Unsure about how to deal with non-terminal rewards or when exactly they occur
+        assert all(r == 0 for r in state.rewards) or state.is_terminal
 
-for pid in rl_pids:
-    players[pid].reset_new_game()
+        if state.is_terminal:
+            break
 
-for _ in range(C.train.max_steps_per_episode):
-    # Unsure about how to deal with non-terminal rewards or when exactly they occur
-    assert all(r == 0 for r in state.rewards) or state.is_terminal
+        if state.is_chance:
+            chance_outcomes = state.chance_outcomes
+            action = rng.choice(C.game.instance.max_num_actions, p=chance_outcomes)
+            state.apply_action(action)
+            for tid, traj in trajectories.items():
+                node = get_update_mcts_tree(tid, action)
+                traj.append(
+                    TrajectoryState(
+                        observation=None,
+                        latent_rep=node.latent_rep,
+                        old_beliefs=players[tid].beliefs,
+                        dyn_beliefs=node.beliefs,
+                        player_type=PlayerType.Chance,
+                        action=action,
+                        target_policy=chance_outcomes,
+                        value=node.value,
+                        reward=C.game.calculate_reward(state.rewards, tid),
+                    )
+                )
+            continue
 
-    if state.is_terminal:
-        break
+        pid = state.current_player
+        if pid in rl_pids:
+            obs = state.observation
+            action, old_beliefs, root_node = players[pid].request_action(obs)
+            target_policy = C.mcts.get_node_target_policy(root_node)
+            mcts_nodes[pid] = root_node
+        else:
+            action = players[pid].request_action(state, C.game.instance)
 
-    if state.is_chance:
-        chance_outcomes = state.chance_outcomes
-        action = rng.choice(C.game.instance.max_num_actions, p=chance_outcomes)
+        # estimate opponent behavior by averaging over their single moves:
+        move_onehot = F.one_hot(torch.tensor(action), C.game.instance.max_num_actions)
         state.apply_action(action)
+
         for tid, traj in trajectories.items():
-            node = get_update_mcts_tree(tid, action)
-            traj.append(
-                TrajectoryState(
+            if tid == pid:
+                ts = TrajectoryState(
+                    observation=obs,
+                    latent_rep=None,
+                    old_beliefs=old_beliefs,
+                    dyn_beliefs=None,
+                    player_type=PlayerType.Self,
+                    action=action,
+                    target_policy=target_policy,
+                    value=root_node.value,
+                    reward=C.game.calculate_reward(state.rewards, pid),
+                )
+            else:
+                node = get_update_mcts_tree(tid, action)
+                ts = TrajectoryState(
                     observation=None,
                     latent_rep=node.latent_rep,
                     old_beliefs=players[tid].beliefs,
                     dyn_beliefs=node.beliefs,
-                    player_type=PlayerType.Chance,
+                    player_type=PlayerType.Teammate
+                    if C.player.is_teammate(pid, tid)
+                    else PlayerType.Opponent,
                     action=action,
-                    target_policy=chance_outcomes,
+                    target_policy=move_onehot,
                     value=node.value,
                     reward=C.game.calculate_reward(state.rewards, tid),
                 )
-            )
-        continue
+            traj.append(ts)
 
-    pid = state.current_player
-    if pid in rl_pids:
-        obs = state.observation
-        action, old_beliefs, root_node = players[pid].request_action(obs)
-        target_policy = C.mcts.get_node_target_policy(root_node)
-        mcts_nodes[pid] = root_node
-    else:
-        action = players[pid].request_action(state, C.game.instance)
-
-    # estimate opponent behavior by averaging over their single moves:
-    move_onehot = F.one_hot(torch.tensor(action), C.game.instance.max_num_actions)
-    state.apply_action(action)
-
-    for tid, traj in trajectories.items():
-        if tid == pid:
-            ts = TrajectoryState(
-                observation=obs,
-                latent_rep=None,
-                old_beliefs=old_beliefs,
-                dyn_beliefs=None,
-                player_type=PlayerType.Self,
-                action=action,
-                target_policy=target_policy,
-                value=root_node.value,
-                reward=C.game.calculate_reward(state.rewards, pid),
-            )
-        else:
-            node = get_update_mcts_tree(tid, action)
-            ts = TrajectoryState(
-                observation=None,
-                latent_rep=node.latent_rep,
-                old_beliefs=players[tid].beliefs,
-                dyn_beliefs=node.beliefs,
-                player_type=PlayerType.Teammate
-                if C.player.is_teammate(pid, tid)
-                else PlayerType.Opponent,
-                action=action,
-                target_policy=move_onehot,
-                value=node.value,
-                reward=C.game.calculate_reward(state.rewards, tid),
-            )
-        traj.append(ts)
-
-for traj in trajectories:
-    replay_buffer.add_trajectory(traj)
+    for traj in trajectories:
+        replay_buffer.add_trajectory(traj)
