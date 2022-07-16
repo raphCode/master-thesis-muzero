efd1c6a0598bf6a90056617b6ee7fbdc1fa5dcc8
raphTec <git@raphtec.net>
2022-07-16 17:02:37 +0200

    RLPlayer.request_action(): Fix variable name

--- rl_player.py
+++ rl_player.py
@@ -31,7 +31,7 @@ class RLPlayer:
         """
         old_beliefs = self.beliefs.to(device="cpu", copy=True)
         latent_rep, self.beliefs = C.nets.representation.si(obs, self.beliefs)
-        root_node = run_mcts(latent_rep, new_beliefs)
+        root_node = run_mcts(latent_rep, self.beliefs)
         action = C.mcts.get_node_action(root_node)
         return RLPResult(action, old_beliefs, root_node)
 
