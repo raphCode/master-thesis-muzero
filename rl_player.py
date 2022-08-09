327b8de77a26a1912fc49a3e26052ccc79f3e07e
raphTec <git@raphtec.net>
2022-08-09 13:46:25 +0200

    RLPlayer: Track and forward move number to get_node_action()

--- rl_player.py
+++ rl_player.py
@@ -20,6 +20,7 @@ class RLPlayer:
     """
 
     beliefs: torch.Tensor
+    move_number: int
 
     def __init__(self):
         self.reset_new_game()
@@ -33,8 +34,10 @@ class RLPlayer:
         latent_rep, self.beliefs = C.nets.representation.si(obs, self.beliefs)
         # TODO: instead of re-running, try to reuse previous tree search from selfplay here
         root_node = run_mcts(latent_rep, self.beliefs)
-        action = C.mcts.get_node_action(root_node)
+        action = C.mcts.get_node_action(root_node, self.move_number)
+        self.move_number += 1
         return RLPResult(action, old_beliefs, root_node)
 
     def reset_new_game(self):
         self.beliefs = C.nets.initial_beliefs.detach().clone()
+        self.move_number = 0
