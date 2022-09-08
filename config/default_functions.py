dbeb36af3098c2ac18989f87ae38e553e7193b53
raphTec <git@raphtec.net>
2022-09-08 21:36:09 +0200

    selection_score_muzero_ucb(): Fix formatting

--- config/default_functions.py
+++ config/default_functions.py
@@ -48,16 +48,20 @@ def target_policy_visit_count(node: Node) -> Sequence[float]:
 
 def selection_score_muzero_ucb(node: Node) -> float:
     prior_scale = (
-        math.log(
-            (
-                node.parent.visit_count
-                + C.mcts.selection_score_muzero_ucb.prior_log_scale_base
-                + 1
+        (
+            math.log(
+                (
+                    node.parent.visit_count
+                    + C.mcts.selection_score_muzero_ucb.prior_log_scale_base
+                    + 1
+                )
+                / C.mcts.selection_score_muzero_ucb.prior_log_scale_base
             )
-            / C.mcts.selection_score_muzero_ucb.prior_log_scale_base
+            + C.mcts.selection_score_muzero_ucb.prior_log_scale_init
         )
-        + C.mcts.selection_score_muzero_ucb.prior_log_scale_init
-    ) * math.sqrt(node.parent.visit_count) / (node.visit_count + 1)
+        * math.sqrt(node.parent.visit_count)
+        / (node.visit_count + 1)
+    )
     prior_score = node.prior * prior_scale
     if not node.is_expanded:
         return prior_score
