801d187a40d295bf9db014ba89c82062892de534
raphTec <git@raphtec.net>
2022-09-08 22:08:53 +0200

    Organize muzero ucb score into functions/muzero.py

--- config/default_functions.py
+++ config/default_functions.py
@@ -1,4 +1,3 @@
-import math
 from collections.abc import Sequence
 
 import numpy as np
@@ -28,26 +27,3 @@ def action_visit_count(node: Node, move_number: int) -> int:
 def target_policy_visit_count(node: Node) -> Sequence[float]:
     visit_counts = [child.visit_count for child in node.children]
     return softmax(visit_counts, C.mcts.target_policy_visit_count.softmax_temp)
-
-
-def selection_score_muzero_ucb(node: Node) -> float:
-    prior_scale = (
-        (
-            math.log(
-                (
-                    node.parent.visit_count
-                    + C.mcts.selection_score_muzero_ucb.prior_log_scale_base
-                    + 1
-                )
-                / C.mcts.selection_score_muzero_ucb.prior_log_scale_base
-            )
-            + C.mcts.selection_score_muzero_ucb.prior_log_scale_init
-        )
-        * math.sqrt(node.parent.visit_count)
-        / (node.visit_count + 1)
-    )
-    prior_score = node.prior * prior_scale
-    if not node.is_expanded:
-        return prior_score
-    value_score = node.reward + node.value * C.train.discount_factor
-    return value_score + prior_score
---
+++ functions/muzero.py
@@ -0,0 +1,26 @@
+import math
+from collections.abc import Callable, Iterable, Sequence
+
+import numpy as np
+
+from mcts import Node
+from config import config as C
+
+
+def ucb_score(node: Node) -> float:
+    prior_scale = (
+        (
+            math.log(
+                (node.parent.visit_count + C.mcts.muzero_ucb.prior_log_scale_base + 1)
+                / C.mcts.muzero_ucb.prior_log_scale_base
+            )
+            + C.mcts.muzero_ucb.prior_log_scale_init
+        )
+        * math.sqrt(node.parent.visit_count)
+        / (node.visit_count + 1)
+    )
+    prior_score = node.prior * prior_scale
+    if not node.is_expanded:
+        return prior_score
+    value_score = node.reward + node.value * C.train.discount_factor
+    return value_score + prior_score
