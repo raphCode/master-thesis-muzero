0f05775513da0f764574f9d25104a1d41b9d3a5a
raphTec <git@raphtec.net>
2022-09-08 16:32:59 +0200

    train.py: Batch training and fix unroll bugs
    The bug was that the network interferences were unrolled in the
    incorrect order: First, the predictions for the current state have to be
    made, then the dynamics inference to get to the next state. Only then
    the latent may be compared to the observation of the next state.

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
+import attrs
 import torch
 import torch.nn.functional as F
 from attrs import Factory, define
+from torch.utils.tensorboard import SummaryWriter
 
 from config import config as C
-from trajectory import TrajectoryState
+from trajectory import PlayerType, TrainingData
 
 
 @define
@@ -15,58 +17,69 @@ class Losses:
     player_type: torch.Tensor = 0
 
 
-def process_trajectory(traj: list[TrajectoryState], losses: Losses):
+@define
+class LossDataCounts:
+    fit: int = 0
+    latent: int = 0
+
+
+def process_batch(batch: list[TrainingData], sw: SummaryWriter, n: int):
     # TODO: move tensors to GPU
+    losses = Losses()
+    counts = LossDataCounts()
 
-    first = traj[0]
-    if first.observation is None:
-        beliefs = first.dyn_beliefs
-        latent_rep = first.latent_rep
-    else:
-        latent_rep, beliefs = C.nets.representation.si(
-            first.observation, first.old_beliefs
-        )
+    first = batch[0]
+    obs_latent_rep, obs_beliefs = C.nets.representation(*first.observation, first.beliefs)
+    latent_rep = obs_latent_rep.where(
+        first.is_observation.unsqueeze(-1), first.latent_rep
+    )
+    beliefs = obs_beliefs.where(first.is_observation.unsqueeze(-1), first.beliefs)
 
-    for ts in traj:
-        latent_rep, beliefs, reward = C.nets.dynamics.si(
-            latent_rep,
-            beliefs,
-            F.one_hot(torch.tensor(ts.action), C.game.instance.max_num_actions),
-        )
-        losses.reward += F.l1_loss(
-            reward, torch.tensor(ts.reward, dtype=torch.float).view(1)
-        )
+    for step in batch:
+        if not step.is_data.any():
+            break
 
-        if ts.observation is not None:
-            new_latent_rep, new_beliefs = C.nets.representation.si(
-                ts.observation, beliefs
+        if step is not first and step.is_observation.any():
+            obs_latent_rep, obs_beliefs = C.nets.representation(
+                *step.observation, beliefs
             )
-            losses.latent += F.cosine_embedding_loss(
-                latent_rep, new_latent_rep, torch.tensor(1)
+            losses.latent += (
+                F.mse_loss(latent_rep, obs_latent_rep, reduction="none")
+                .mean(dim=1)
+                .masked_select(step.is_observation)
+                .sum()
             )
-            # TODO: losses here are not properly averaged: this branch is not taken for all items in the batch
+            counts.latent += step.is_observation.count_nonzero()
 
-        value, policy, player_type = C.nets.prediction.si(
+        counts.fit += step.is_data.count_nonzero()
+
+        value, policy_logits, player_type_logits = C.nets.prediction(
             latent_rep, beliefs, logits=True
         )
-        losses.value += F.l1_loss(
-            value, torch.tensor(ts.value, dtype=torch.float).view(1)
-        )
+        losses.value += F.mse_loss(value, step.value_target, reduction="none")[
+            step.is_data
+        ].sum()
         losses.policy += F.cross_entropy(
-            policy.unsqueeze(0),
-            torch.tensor(ts.target_policy, dtype=torch.float).unsqueeze(0),
-        )
-        # TODO: correct for class imbalance?
+            policy_logits, step.target_policy, reduction="none"
+        )[step.is_data].sum()
         losses.player_type += F.cross_entropy(
-            player_type, torch.tensor(ts.player_type, dtype=int)
+            player_type_logits, step.player_type, reduction="none"
+        )[step.is_data].sum()
+
+        latent_rep, beliefs, reward = C.nets.dynamics(
+            latent_rep, beliefs, step.action_onehot
         )
+        losses.reward += F.mse_loss(reward, step.reward, reduction="none")[
+            step.is_data
+        ].sum()
 
-
-def process_batch(batch: list[list[TrajectoryState]], sw: SummaryWriter, n: int):
-    losses = Losses()
-    for traj in batch:
-        # TODO: batch network inference
-        process_trajectory(traj, losses)
+    if counts.latent > 0:
+        losses.latent /= counts.latent
+    if counts.fit > 0:
+        losses.value /= counts.fit
+        losses.policy /= counts.fit
+        losses.reward /= counts.fit
+        losses.player_type /= counts.fit
 
     for k, loss in attrs.asdict(losses).items():
         sw.add_scalar(f"loss/{k}", loss, n)
@@ -77,7 +90,7 @@ def process_batch(batch: list[list[TrajectoryState]], sw: SummaryWriter, n: int)
         + C.train.loss_weights.reward * losses.reward
         + C.train.loss_weights.policy * losses.policy
         + C.train.loss_weights.player_type * losses.player_type
-    ) / bsize
+    )
     C.train.optimizer.zero_grad()
     loss.backward()
     C.train.optimizer.step()
