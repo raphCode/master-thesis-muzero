d9b5580503b3e16b781cfab4261786defdf113b8
raphTec <git@raphtec.net>
2022-09-21 18:37:52 +0200

    Move + rename function for game reward into new file

--- fn/default.py
+++ fn/default.py
@@ -1,14 +1,2 @@
-from collections.abc import Callable, Iterable, Sequence
-
-import numpy as np
-
-from mcts import Node
-from config import config as C
-
-
-def standard_reward(rewards: tuple[float], player_id: int) -> float:
-    return rewards[player_id]
-
-
 def no_teammate(pid_a: int, pid_b: int) -> bool:
     return False
---
+++ fn/reward.py
@@ -0,0 +1,2 @@
+def default(rewards: tuple[float], player_id: int) -> float:
+    return rewards[player_id]
