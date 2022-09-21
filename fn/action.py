663676d879dc57cbfa696af34fc52e6004add70b
raphTec <git@raphtec.net>
2022-09-21 18:16:20 +0200

    fn/action.py: Extract softmax temp interpolation into function

--- fn/action.py
+++ fn/action.py
@@ -8,35 +8,30 @@ from .util import softmax, get_values_where_expanded
 rng = np.random.default_rng()
 
 
-def from_visit_count(node: Node, move_number: int) -> int:
-    visit_counts = [child.visit_count for child in node.children]
-    temp = np.interp(
+def _interp_softmax_temp(move_number: int, cfg_name: str) -> float:
+    cfg = getattr(C.mcts.fn.action, cfg_name)
+    return np.interp(
         move_number,
         (
-            C.mcts.fn.action.from_visit_count.num_moves_start,
-            C.mcts.fn.action.from_visit_count.num_moves_end,
+            cfg.move_num_start,
+            cfg.move_num_end,
         ),
         (
-            C.mcts.fn.action.from_visit_count.softmax_temp_start,
-            C.mcts.fn.action.from_visit_count.softmax_temp_end,
+            cfg.softmax_temp_start,
+            cfg.softmax_temp_end,
         ),
     )
+
+
+def from_visit_count(node: Node, move_number: int) -> int:
+    visit_counts = [child.visit_count for child in node.children]
+    temp = _interp_softmax_temp(move_number, "from_visit_count")
     return rng.choice(C.game.instance.max_num_actions, p=softmax(visit_counts, temp))
 
 
 def from_visit_count_expanded(node: Node, move_number: int) -> int:
     visit_counts, idx = get_values_where_expanded(node.children, lambda n: n.visit_count)
-    temp = np.interp(
-        move_number,
-        (
-            C.mcts.fn.action.from_visit_count.num_moves_start,
-            C.mcts.fn.action.from_visit_count.num_moves_end,
-        ),
-        (
-            C.mcts.fn.action.from_visit_count.softmax_temp_start,
-            C.mcts.fn.action.from_visit_count.softmax_temp_end,
-        ),
-    )
+    temp = _interp_softmax_temp(move_number, "from_visit_count")
     probs = np.full(len(node.children), 0.0)
     probs[idx] = softmax(visit_counts, temp)
     return rng.choice(C.game.instance.max_num_actions, p=probs)
