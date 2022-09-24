4f61a0c92a2c0b5f44a538502edaf604e9d85861
raphTec <git@raphtec.net>
2022-09-24 15:13:40 +0200

    main(): Set up G.nets

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from omegaconf import MISSING, DictConfig
 from hydra.core.config_store import ConfigStore
 
 import config
+from config import C
+from globals import G
 from config.schema import GameSchema, MctsSchema, TrainSchema, PlayerSchema, NetworkSchema
+from networks.bases import Networks
 
 cs = ConfigStore.instance()
 cs.store(name="hydra_job_config", group="hydra.job", node={"chdir": True})
@@ -36,6 +39,13 @@ cs.store(name="base_config", node=BaseConfig)
 def main(cfg: DictConfig):
     config.populate_config(cfg)
     config.save_source_code()
+    G.nets = Networks(
+        representation=C.nets.factory.representation(),
+        prediction=C.nets.factory.prediction(),
+        dynamics=C.nets.factory.dynamics(),
+        initial_latent_rep=C.nets.factory.initial_latent_rep(),
+        initial_beliefs=C.nets.factory.initial_beliefs(),
+    )
 
 
 if __name__ == "__main__":
