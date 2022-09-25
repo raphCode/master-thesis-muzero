9eef3686ddd4925f16ee89edaf8b30041ce8924c
raphTec <git@raphtec.net>
2022-09-25 11:08:34 +0200

    train.py: Wrap in Trainer class
    This is done so that the optimizer can be created locally in the class.
    Also this is in preparation for the multiprocessing so that the Trainer
    class can become a Ray actor.
    
    This commit involves a space change so the diff is best viewed with
    --ignore-space-change.

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ from attrs import define
 from torch.utils.tensorboard import SummaryWriter
 
 from config import C
-from globals import G
 from trajectory import TrainingData
+from networks.bases import Networks
 
 
 @define
@@ -24,76 +24,83 @@ class LossDataCounts:
     latent: int = 0
 
 
-def process_batch(batch: list[TrainingData], sw: SummaryWriter, n: int):
-    # TODO: move tensors to GPU
-    losses = Losses()
-    counts = LossDataCounts()
+class Trainer:
+    def __init__(self, nets: Networks):
+        self.nets = nets
+        self.optimizer = C.train.optimizer_factory(self.nets)
 
-    first = batch[0]
-    obs_latent_rep, obs_beliefs = G.nets.representation(*first.observation, first.beliefs)
-    latent_rep = obs_latent_rep.where(
-        first.is_observation.unsqueeze(-1), first.latent_rep
-    )
-    beliefs = obs_beliefs.where(first.is_observation.unsqueeze(-1), first.beliefs)
+    def process_batch(self, batch: list[TrainingData], sw: SummaryWriter, n: int):
+        # TODO: move tensors to GPU
+        losses = Losses()
+        counts = LossDataCounts()
 
-    for step in batch:
-        if not step.is_data.any():
-            break
-
-        if step is not first and step.is_observation.any():
-            obs_latent_rep, obs_beliefs = G.nets.representation(
-                *step.observation, beliefs
-            )
-            losses.latent += (
-                F.mse_loss(latent_rep, obs_latent_rep, reduction="none")
-                .mean(dim=1)
-                .masked_select(step.is_observation)
-                .sum()
-            )
-            counts.latent += step.is_observation.count_nonzero()
-
-        counts.fit += step.is_data.count_nonzero()
-
-        value, policy_logits, player_type_logits = G.nets.prediction(
-            latent_rep, beliefs, logits=True
+        first = batch[0]
+        obs_latent_rep, obs_beliefs = self.nets.representation(
+            *first.observation, first.beliefs
         )
-        losses.value += F.mse_loss(value, step.value_target, reduction="none")[
-            step.is_data
-        ].sum()
-        losses.policy += F.cross_entropy(
-            policy_logits, step.target_policy, reduction="none"
-        )[step.is_data].sum()
-        losses.player_type += F.cross_entropy(
-            player_type_logits, step.player_type, reduction="none"
-        )[step.is_data].sum()
-
-        latent_rep, beliefs, reward = G.nets.dynamics(
-            latent_rep, beliefs, step.action_onehot
+        latent_rep = obs_latent_rep.where(
+            first.is_observation.unsqueeze(-1), first.latent_rep
         )
-        losses.reward += F.mse_loss(reward, step.reward, reduction="none")[
-            step.is_data
-        ].sum()
+        beliefs = obs_beliefs.where(first.is_observation.unsqueeze(-1), first.beliefs)
 
-    if counts.latent > 0:
-        losses.latent /= counts.latent
-    if counts.fit > 0:
-        losses.value /= counts.fit
-        losses.policy /= counts.fit
-        losses.reward /= counts.fit
-        losses.player_type /= counts.fit
+        for step in batch:
+            if not step.is_data.any():
+                break
 
-    for k, loss in attrs.asdict(losses).items():
-        sw.add_scalar(f"loss/{k}", loss, n)
+            if step is not first and step.is_observation.any():
+                obs_latent_rep, obs_beliefs = self.nets.representation(
+                    *step.observation, beliefs
+                )
+                losses.latent += (
+                    F.mse_loss(latent_rep, obs_latent_rep, reduction="none")
+                    .mean(dim=1)
+                    .masked_select(step.is_observation)
+                    .sum()
+                )
+                counts.latent += step.is_observation.count_nonzero()
 
-    loss = (
-        C.train.loss_weights.latent * losses.latent
-        + C.train.loss_weights.value * losses.value
-        + C.train.loss_weights.reward * losses.reward
-        + C.train.loss_weights.policy * losses.policy
-        + C.train.loss_weights.player_type * losses.player_type
-    )
-    C.train.optimizer.zero_grad()
-    loss.backward()
-    C.train.optimizer.step()
+            counts.fit += step.is_data.count_nonzero()
 
-    sw.add_scalar("loss/total", loss, n)
+            value, policy_logits, player_type_logits = self.nets.prediction(
+                latent_rep, beliefs, logits=True
+            )
+            losses.value += F.mse_loss(value, step.value_target, reduction="none")[
+                step.is_data
+            ].sum()
+            losses.policy += F.cross_entropy(
+                policy_logits, step.target_policy, reduction="none"
+            )[step.is_data].sum()
+            losses.player_type += F.cross_entropy(
+                player_type_logits, step.player_type, reduction="none"
+            )[step.is_data].sum()
+
+            latent_rep, beliefs, reward = self.nets.dynamics(
+                latent_rep, beliefs, step.action_onehot
+            )
+            losses.reward += F.mse_loss(reward, step.reward, reduction="none")[
+                step.is_data
+            ].sum()
+
+        if counts.latent > 0:
+            losses.latent /= counts.latent
+        if counts.fit > 0:
+            losses.value /= counts.fit
+            losses.policy /= counts.fit
+            losses.reward /= counts.fit
+            losses.player_type /= counts.fit
+
+        for k, loss in attrs.asdict(losses).items():
+            sw.add_scalar(f"loss/{k}", loss, n)
+
+        loss = (
+            C.train.loss_weights.latent * losses.latent
+            + C.train.loss_weights.value * losses.value
+            + C.train.loss_weights.reward * losses.reward
+            + C.train.loss_weights.policy * losses.policy
+            + C.train.loss_weights.player_type * losses.player_type
+        )
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        sw.add_scalar("loss/total", loss, n)
