34bc6648cb9f575a01fa95eaac4e2d91d17c83e2
raphTec <git@raphtec.net>
2022-12-12 15:12:06 +0100

    Add chapter and TOC commands to thesis class

--- thesis.lua
+++ thesis.lua
@@ -26,12 +26,80 @@ class.defaultFrameset = {
 
 function class:_init(options)
   plain._init(self, options)
+  self:loadPackage("tableofcontents")
   self:loadPackage("footnotes", {
       insertInto = "footnotes",
       stealFrom = { "content" }
     })
   SILE.call("font", { family = "Latin Modern Roman", size = "12pt" })
   SILE.call("neverindent")
+  self:redefineCommands()
+end
+
+function class:redefineCommands ()
+  local original_toc_fn = SILE.Commands["tableofcontents"]
+  self:registerCommand("tableofcontents", function (options, _)
+    SILE.call("thesis:chapterfont", _, function () original_toc_fn(options, _) end)
+  end)
+
+  self:registerCommand("tableofcontents:header", function (_, _)
+    SILE.call("thesis:chapterfont", {}, function ()
+      SILE.call("fluent", {}, { "tableofcontents-title" })
+    end)
+    SILE.call("medskip")
+  end)
+
+  self:registerCommand("tableofcontents:level1item", function (_, content)
+    SILE.call("bigskip")
+    local original_dotfill = SILE.Commands["dotfill"]
+    SILE.Commands["dotfill"] = SILE.Commands["hfill"]  -- hack to have a blank hfill
+    SILE.call("font", { size = 14, weight = 800 }, content)
+    SILE.Commands["dotfill"] = original_dotfill
+    SILE.call("medskip")
+  end)
+end
+
+function class:registerCommands ()
+  plain.registerCommands(self)
+
+  local function sectioning(options, content)
+    local number
+    if SU.boolean(options.numbering, true) then
+      SILE.call("increment-multilevel-counter", { id = "sectioning", level = options.level })
+      number = self.packages.counters:formatMultilevelCounter(self:getMultilevelCounter("sectioning"))
+      SILE.call("show-multilevel-counter", { id = "sectioning" })
+    end
+    if SU.boolean(options.toc, true) then
+      SILE.call("tocentry", { level = options.level, number = number }, SU.subContent(content))
+    end
+    SILE.process( { " " } )
+    SILE.process(content)
+  end
+
+  self:registerCommand("chapter", function (options, content)
+    SILE.call("thesis:newpage")
+    SILE.call("set-counter", { id = "footnote", value = 1 })
+    SILE.call("thesis:chapterfont", {}, function ()
+      sectioning({
+        numbering = options.numbering,
+        toc = options.toc,
+        level = 1,
+      }, content)
+    end)
+    SILE.call("par")
+    SILE.call("bigskip")
+  end, "Begin a new chapter")
+
+  self:registerCommand("thesis:newpage", function (_, content)
+    SILE.typesetter:leaveHmode()
+    SILE.call("supereject")  -- prevent "underfull frame" warnings by vfilling page
+    SILE.call("hbox")  -- prevents the skip to disappear at the top of the page
+    SILE.call("skip", { height = "20mm" } )
+  end)
+
+  self:registerCommand("thesis:chapterfont", function (_, content)
+    SILE.call("font", { family = "Latin Modern Sans", size = "24pt", weight = 10000 }, content)
+  end)
 end
 
 return class
