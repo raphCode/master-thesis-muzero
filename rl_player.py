fe1a15be18ac06c6d7babb24ec00e1587fbab661
raphTec <git@raphtec.net>
2023-02-02 16:29:00 +0100

    Remove old RLPlayer implementation
    This is really outdated, committing the rewritten stuff on top clutters
    the diff, so removing that.

--- rl_player.py
+++ rl_player.py
@@ -2,39 +2,8 @@ from typing import NamedTuple
 
 import torch
 
-from mcts import run_mcts
-from config import C
-
 
 class RLPResult(NamedTuple):
     action: int
     old_beliefs: torch.Tensor
     mcts_value: float
-
-
-class RLPlayer:
-    """
-    Player which does MCTS with neural networks for move selection.
-    Don't share a single RLPlayer instance for multiple players because of the private
-    beliefs state.
-    """
-
-    beliefs: torch.Tensor
-    move_number: int
-
-    def request_action(self, observation: tuple[torch.Tensor]) -> RLPResult:
-        """
-        Requests an action from RL agent for the current observation.
-        Returns an action to choose as well as extra data for recording a game trajectory.
-        """
-        old_beliefs = self.beliefs.to(device="cpu", copy=True)
-        latent_rep, self.beliefs = G.nets.representation.si(*observation, self.beliefs)
-        # TODO: instead of re-running, try to reuse previous tree search from selfplay here
-        root_node = run_mcts(latent_rep, self.beliefs)
-        action = C.mcts.node_action_fn(root_node, self.move_number)
-        self.move_number += 1
-        return RLPResult(action, old_beliefs, root_node)
-
-    def reset_new_game(self):
-        self.beliefs = G.nets.initial_beliefs.detach().clone()
-        self.move_number = 0
