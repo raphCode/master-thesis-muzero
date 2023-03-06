73d3bc0209abd7555e04542744feb731bb2655af
raphTec <git@raphtec.net>
2023-03-06 12:29:58 +0100

    Change reward fns to accept a GameState
    This is more flexible since the GameState allows access to the rewards
    as well as the team configuration.

--- fn/reward.py
+++ fn/reward.py
@@ -1,12 +1,14 @@
 from typing import TypeAlias
 from collections.abc import Callable
 
-RewardFn: TypeAlias = Callable[[tuple[float, ...], int], float]
+from games.bases import GameState
+
+RewardFn: TypeAlias = Callable[[GameState, int], float]
 
 
 def assert_fn_type(fn: RewardFn) -> None:
     pass
 
 
-def default(rewards: tuple[float, ...], player_id: int) -> float:
-    return rewards[player_id]
+def default(state: GameState, player_id: int) -> float:
+    return state.rewards[player_id]
