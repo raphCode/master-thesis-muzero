414950297cc8d0e6d7a39eafaf06a4bc1ee95560
raphTec <git@raphtec.net>
2023-03-06 12:30:49 +0100

    Add typechecking to fn/reward.py

--- fn/reward.py
+++ fn/reward.py
@@ -7,8 +7,12 @@ RewardFn: TypeAlias = Callable[[GameState, int], float]
 
 
 def assert_fn_type(fn: RewardFn) -> None:
+    """For typechecking."""
     pass
 
 
 def default(state: GameState, player_id: int) -> float:
     return state.rewards[player_id]
+
+
+assert_fn_type(default)
