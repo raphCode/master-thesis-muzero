6cbeaa1ae3e0891ee672fec3a7dc19180eeb8edb
raphTec <git@raphtec.net>
2023-03-25 14:22:25 +0100

    Add TensorCache class
    This should be useful to reduce memory usage for reoccuring tensors, in
    e.g. the replay buffer.

--- util.py
+++ util.py
@@ -1,4 +1,8 @@
-from typing import TypeVar, Callable, Optional
+import functools
+from typing import TypeVar, Callable, Optional, cast
+
+import torch
+import torch.nn.functional as F
 
 A = TypeVar("A")
 B = TypeVar("B")
@@ -6,3 +10,21 @@ B = TypeVar("B")
 
 def optional_map(f: Callable[[A], B]) -> Callable[[Optional[A]], Optional[B]]:
     return lambda x: f(x) if x is not None else None
+
+
+T = TypeVar("T")
+
+
+class TensorCache:
+    """
+    Creates and caches tensors to reduce memory and garbage collector pressure.
+    This is a class so the caches can be dropped by deleting the instance.
+    """
+
+    def __init__(self) -> None:
+        self.tensor = functools.lru_cache(maxsize=None, typed=True)(torch.tensor)
+        self.zeros = functools.cache(torch.zeros)
+
+    @functools.cache
+    def onehot(self, index: int | torch.Tensor, num_classes: int) -> torch.LongTensor:
+        return cast(torch.LongTensor, F.one_hot(self.tensor(index), num_classes))
