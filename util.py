11dc518761b8d3f74edb3b0812b12b0662630657
raphTec <git@raphtec.net>
2023-03-26 15:50:10 +0200

    Add RingBuffer implementation
    The replay buffer needs a storage for trajectories with:
    - fast appends
    - maximum size, adding new elements discards the oldest ones
    - fast random access
    
    A ring buffer provides these features in O(1).

--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 import functools
-from typing import TypeVar, Callable, Optional, cast
+from typing import Generic, TypeVar, Callable, Optional, cast
 
 import torch
 import torch.nn.functional as F
@@ -28,3 +28,31 @@ class TensorCache:
     @functools.cache
     def onehot(self, index: int | torch.Tensor, num_classes: int) -> torch.LongTensor:
         return cast(torch.LongTensor, F.one_hot(self.tensor(index), num_classes))
+
+
+class RingBuffer(Generic[T]):
+    """
+    Storage with maximum size, append() and random access in O(1).
+    New items overwrite old ones after the buffer grew to maximum size.
+    """
+
+    def __init__(self, size: int):
+        self.data: list[T] = []
+        self.size = size
+        self.position = 0
+
+    def append(self, value: T) -> None:
+        if len(self.data) < self.size:
+            self.data.append(value)
+        else:
+            self.data[self.position] = value
+        self.position = (self.position + 1) % self.size
+
+    def __getitem__(self, index: int) -> T:
+        """
+        Index 0 returns the oldest item, -1 the newest.
+        """
+        return self.data[(index + self.position) % len(self.data)]
+
+    def __len__(self) -> int:
+        return len(self.data)
