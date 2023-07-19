b325fb957549493fe39b1dff46ec67158cdc0550
raphTec <git@raphtec.net>
2023-07-19 13:38:57 +0200

    Offer to delete output folder of short aborted runs
    This helps uncluttering the tensorboard.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+import shutil
 import logging
 from typing import TYPE_CHECKING
 
@@ -32,6 +34,21 @@ log = logging.getLogger("main")
 
 @hydra.main(version_base=None, config_path="run_config", config_name="base_config")
 def main(cfg: DictConfig) -> None:
+    def ask_delete_logs() -> None:
+        cwd = os.getcwd()
+        orig_cwd = hydra.utils.get_original_cwd()
+        assert cwd.startswith(orig_cwd) and len(cwd) > len(orig_cwd)
+        assert "outputs" in cwd
+        assert os.path.exists("tb")
+        assert os.path.exists(".hydra")
+        msg = (
+            "\nAborted a short run."
+            f"\nThe run created tensorboard event files and various logs in:\n{cwd}"
+            "\nDo you want to delete this directory? [N/y] "
+        )
+        if input(msg) == "y":
+            shutil.rmtree(cwd)
+
     logging.captureWarnings(True)
     populate_config(cfg)
 
@@ -39,18 +56,22 @@ def main(cfg: DictConfig) -> None:
     rb = ReplayBuffer()
     t = Trainer(pc.net)
     n = 0
-    with TensorboardLogger(log_dir="tb") as tb:
-        while n < 100_000:
-            with torch.no_grad():
-                result = run_episode(pc, tb.create_step_logger(n))
-            n += result.moves
-            for traj in result.trajectories:
-                rb.add_trajectory(traj, result.game_completed)
-            pc.net.update_rescalers(rb)
-            batch_samples = C.training.batch_size * C.training.max_trajectory_length
-            target_samples = rb.data_added * C.training.train_selfplay_ratio
-            while rb.data_sampled < target_samples - batch_samples:
-                t.process_batch(rb.sample(), tb.create_step_logger(n))
+    try:
+        with TensorboardLogger(log_dir="tb") as tb:
+            while n < 100_000:
+                with torch.no_grad():
+                    result = run_episode(pc, tb.create_step_logger(n))
+                n += result.moves
+                for traj in result.trajectories:
+                    rb.add_trajectory(traj, result.game_completed)
+                pc.net.update_rescalers(rb)
+                batch_samples = C.training.batch_size * C.training.max_trajectory_length
+                target_samples = rb.data_added * C.training.train_selfplay_ratio
+                while rb.data_sampled < target_samples - batch_samples:
+                    t.process_batch(rb.sample(), tb.create_step_logger(n))
+    except KeyboardInterrupt:
+        if n < 30_000:
+            ask_delete_logs()
 
 
 if __name__ == "__main__":
