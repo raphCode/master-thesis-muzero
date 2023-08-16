4d0b012110cc51de83aacbccea26811425c82ef5
raphTec <git@raphtec.net>
2023-08-16 14:26:02 +0200

    Add debug prints for search tree

--- mcts.py
+++ mcts.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import logging
+import textwrap
 from abc import ABC, abstractmethod
 from enum import Enum
 from typing import TYPE_CHECKING, Self, Literal, Optional, TypeAlias, cast
-from collections.abc import MutableMapping
+from collections.abc import Iterable, MutableMapping
 
 import numpy as np
 import torch
@@ -124,6 +125,28 @@ class Node(ABC):
     def _create_child_at(self, action: int) -> Node:
         pass
 
+    def debug_info(self) -> Iterable[str]:
+        return [
+            f"visits {self.visit_count}",
+            f"pred {np.round(self.value_pred, 2)}",
+            f"reward {np.round(self.reward, 2)}",
+            f"value {np.round(self.value, 2)}",
+        ]
+
+    def debug_description(self) -> str:
+        return "Node"
+
+    def debug_dump_tree(self, maxdepth: int) -> str:
+        if maxdepth <= 1:
+            return repr(self)
+        return repr(self) + "".join(
+            textwrap.indent(f"\n{a}: " + c.debug_dump_tree(maxdepth - 1), 2 * " ")
+            for a, c in sorted(self.children.items())
+        )
+
+    def __repr__(self) -> str:
+        return self.debug_description() + ": " + ", ".join(self.debug_info())
+
 
 class StateNode(Node):
     """
@@ -174,6 +197,14 @@ class StateNode(Node):
             self.mcts,
         )
 
+    def debug_description(self) -> str:
+        if self.player is TurnStatus.CHANCE_PLAYER:
+            return "Chance"
+        elif self.player is self.mcts.own_pid:
+            return "Decision"
+        else:
+            return f"Player {self.player}"
+
 
 class TerminalNode(Node):
     """
@@ -222,6 +253,18 @@ class TerminalNode(Node):
             F.one_hot(torch.tensor(action), C.game.instance.max_num_actions),
         )
         return TerminalNode(latent, np.zeros_like(self.reward), self.mcts)
+
+    def debug_info(self) -> Iterable[str]:
+        return [
+            f"visits {self.visit_count}",
+            f"reward {np.round(self.reward, 2)}",
+        ]
+
+    def debug_description(self) -> str:
+        return "Terminal"
+
+    def debug_dump_tree(self, _: int) -> str:
+        return repr(self)
 
 
 class MCTS:
@@ -305,3 +348,13 @@ class MCTS:
         Replace the root node with its child of the given action.
         """
         self.root = self.root.get_create_child(action)
+
+    def debug_dump_tree(self, maxdepth: int = 5) -> str:
+        return "\n".join(
+            [
+                "Value: " + repr(self.nets.prediction.value_scale),
+                "Reward: " + repr(self.nets.dynamics.reward_scale),
+                f"MC tree for player {self.own_pid}:",
+                self.root.debug_dump_tree(maxdepth),
+            ]
+        )
