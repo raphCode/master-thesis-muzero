6d7fdab8613fdbc390bd4c191362be23619bbecb
raphTec <git@raphtec.net>
2023-08-18 16:05:51 +0200

    Add Map class, observation maps

--- games/carchess/test.py
+++ games/carchess/test.py
@@ -14,6 +14,7 @@ map_path = "maps/map1"
 
 Pos: TypeAlias = tuple[int, int]
 ndarr_bool: TypeAlias = npt.NDArray[np.bool_]
+ndarr_int: TypeAlias = npt.NDArray[np.int]
 
 rng = np.random.default_rng()
 
@@ -54,9 +55,12 @@ class TrafficLights:
         self.lights = mask
         self.reset()
 
-    def reset(self):
-        percent_closed = 0.6
+    def reset(self, percent_closed:float=0.5):
         self.closed = self.lights & (rng.random(self.lights.shape) < percent_closed)
+
+    def toggle_at(self,pos)->None:
+        assert self.lights[pos]
+        self.closed[pos] ^= True
 
     def is_closed(self, pos) -> bool:
         return self.closed[pos]
@@ -65,14 +69,19 @@ class TrafficLights:
         y, x = np.nonzero(self.closed)
         return plt.scatter(x, -y, marker="x", s=200, color="red")
 
+    @property
+    def observation_map(self) -> ndarr_int:
+        return self.lights * (self.closed.astype(int) * 2 - 1)
+
 
 class Layer:
     size: tuple[int, int]
     pos: list[Pos]
     cars: set[int]  # indices of cars along the path
     moveable_cars: set[int]
+    spawn_counter:int
 
-    def __init__(self, mask: ndarr_bool, prepopulate: float = 0.5):
+    def __init__(self, mask: ndarr_bool):
         def is_at_border(x, y) -> bool:
             mx, my = mask.shape
             return x in (0, mx - 1) or y in (0, my - 1)
@@ -115,10 +124,15 @@ class Layer:
                 assert (
                     False
                 ), f"Did not find next field! pos: ({pos[0]}, {pos[1]}), dir: {dir.name}"
+        self.reset()
 
+    def reset(self, prepopulate: float=0.5)->None:
         n = len(self.pos) - 1
         self.cars = set((np.arange(n)[rng.random(n) < prepopulate]))
         self.moveable_cars = set()
+    def update_spawn_count(self,min_spawn:int, max_spawn:int, max_density:float)->None:
+        max_capacity = len(self.pos) -2 # minus first and last field
+        self.spawn_counter=min(int(max_capacity*max_density),self.spawn_counter+ rng.integers(min_spawn, max_spawn, endpoint=True))
 
     @cache
     def offset_pos(self, pos, offset: int = 1) -> Pos:
@@ -143,11 +157,14 @@ class Layer:
     def mark_car(self, pos) -> None:
         self.moveable_cars.add(self.index_at(pos))
 
-    def advance_cars(self) -> None:
+    def advance_cars(self) -> bool:
         self.cars -= self.moveable_cars
         self.cars.update(x + 1 for x in self.moveable_cars)
-        self.cars.discard(len(self.pos) - 1)
+        goal_index =len(self.pos)-1
+        reached_goal = goal_index in self.cars
+        self.cars.discard(goal_index)
         self.moveable_cars = set()
+        return reached_goal
 
     def remove_cars(self, pos: Iterable[Pos]) -> None:
         self.cars.difference_update(map(self.index_at, pos))
@@ -156,36 +173,48 @@ class Layer:
         yield from (self.pos[i] for i in self.cars)
 
     @property
-    def lane_map(self) -> ndarr_bool:
+    def lane_mask(self) -> ndarr_bool:
         ret = np.zeros(self.size, dtype=bool)
         for pos in self.pos:
             ret[pos] = True
         return ret
 
     @property
-    def car_map(self) -> ndarr_bool:
+    def car_mask(self) -> ndarr_bool:
         ret = np.zeros(self.size, dtype=bool)
         for index in self.cars:
             ret[self.pos[index]] = True
         return ret
 
+    @property
+    def observation_map(self) -> ndarr_int:
+        x,y=zip(*self.pos)
+        lane_map = np.full(self.size, -1)
+        lane_map[x,y] = np.linspace(1, 0, len(self.pos))
+        spawn_map = np.zeros(self.size)
+        spawn_map[self.pos[0]] =self.spawn_counter
+        car_map = self.car_mask.astype(int) * 2 - 1
+        return np.stack(lane_map,car_map, spawn_map)
+
+    def plot_grid(self, ax)->Any:
+        ax.set_xticks(np.arange(100) + 0.5)  # , labels="")
+        ax.set_yticks(-np.arange(100) + 0.5)  # , labels="")
+        ax.grid(color="grey")
+        ax.set_aspect("equal")
+
     def plot(self, plt, offset: float, **kwargs: Any) -> List[Any]:
-        def plot(data):
-            y, x = np.asarray(data).T
+        def coords(pos:ndarr_f64):
+            y, x = pos.T
             return x, -y
 
         pos = np.array(self.pos) + offset
-        line_plots = plt.plot(*plot(pos), **kwargs)
+        line_plots = plt.plot(*coords(pos), **kwargs)
         if self.cars:
             cars = np.array([self.pos[c] for c in self.cars]) + offset
-            car_plot = plt.scatter(*plot(cars), **kwargs)
+            car_plot = plt.scatter(*coords(cars), **kwargs)
             return line_plots + [car_plot]
         return line_plots
 
-
-def collision_check(layers: Iterable[Layer]) -> list[Pos]:
-    car_map = sum(l.car_map.astype(int) for l in layers)
-    return list(zip(*np.nonzero(car_map > 1)))
 
 
 layers: list[Layer] = []
@@ -194,11 +223,7 @@ layers: list[Layer] = []
 # ax = iter(axes.flatten())
 # grid = next(ax)
 
-fig, grid = plt.subplots()
-grid.set_xticks(np.arange(100) + 0.5)  # , labels="")
-grid.set_yticks(-np.arange(100) + 0.5)  # , labels="")
-grid.grid(color="grey")
-grid.set_aspect("equal")
+fig, ax = plt.subplots()
 
 im = iio.imread(path.join(map_path, "tl.png"))
 alpha_mask = im[:, :, -1] > 0
@@ -207,48 +232,96 @@ tl = TrafficLights(alpha_mask)
 
 n_maps = 4
 
-colors = ["tab:red", "tab:blue", "tab:cyan", "tab:orange"]
 
 for n in range(n_maps):
     im = iio.imread(path.join(map_path, f"{n + 1}.png"))
-    alpha_mask = im[:, :, -1] > 0
     red_channel = im[:, :, 0]
     layer = Layer(red_channel * alpha_mask)
     layers.append(layer)
 
     continue
-    m = layer.lane_map * 0.2 + layer.car_map
-    next(ax).imshow(m)
 
 
-def can_move(pos: Pos, layer_id: int, layers: list[Layer], tl: TrafficLights):
-    if tl.is_closed(pos):
-        return False
-    next_pos = layers[layer_id].offset_pos(pos)
-    for n, l in enumerate(layers):
-        if l.is_car(next_pos):
-            same_dir = l.dir_at(next_pos) == layers[layer_id].dir_at(pos)
-            same_layer = layer_id == n
-            if same_dir or same_layer:
-                return can_move(next_pos, n, layers, tl)
-            return True  # risk a car crash
-    # empty field
-    return True
 
+class Map:
+    layers:list[Layer]
+    tl:TrafficLights
 
-def resolve_collisions(layers):
-    coll = collision_check(layers)
-    print(coll)
-    for l in layers:
-        l.remove_cars(coll)
+    def __init__(self, map_path: str):
+        # rounds:int = 20,actions_per_round:int=2, reward_car:float=2, reward_crash :float=-1
+        def read_map_img(name:str)->ndarr_int:
+            return iio.imread(path.join(map_path,name))
+        def alpha_mask(img:ndarr_f64)->ndarr_bool:
+            return img[:, :, -1] > 0
+        self.tl=TrafficLights(alpha_mask(read_map_img( "tl.png") ))
+        
+            
 
+    def reset(self, prepopulate: float=0.3,  init_red_rate:float=0.5)->None:
+        for layer in self.layers:
+            layer.reset(prepopulate)
+        self.tl.reset(init_red_rate)
 
-def plot_layers(layers) -> List[Any]:
-    ret = [tl.plot(grid)]
-    for n, l in enumerate(layers):
-        ret.extend(l.plot(grid, offset=(n / n_maps - 0.5) / 2, color=colors[n]))
-    return ret
+    def update_spawn_counts(min_spawn:int, max_spawn:int, max_density:float)->None:
+        # car_density:float=0.5, spawn_rate:tuple[int,int] 
+        for l in self.layers:
+            l.update_spawn_count(min_spawn, max_spawn, max_density)
 
+    def toggle_tl(self,pos)->None:
+        self.tl.toggle_at(pos)
+
+    def simulation_step()->tuple[int,int]:
+        # local function with local cache:
+        # cache is correctly evicted after each simulation step
+        @cache
+        def can_move(pos: Pos, layer_id: int)->bool:
+            """
+            Compute whether a car can move to its next field.
+            Implemented with dynamic programming and recursion.
+            """
+            if self.tl.is_closed(pos):
+                return False
+            layer = self.layers[layer_id]
+            next_pos = layer.offset_pos(pos)
+            for n, l in enumerate(self.layers):
+                if l.is_car(next_pos):
+                    same_dir = l.dir_at(next_pos) == layer.dir_at(pos)
+                    same_layer = layer_id == n
+                    if same_dir or same_layer:
+                        # respect car in next field
+                        return can_move(next_pos, n)
+                    # risk a car crash
+                    return True
+            # empty field
+            return True
+
+        def collision_check() -> tuple[list[Pos], int]:
+            car_map = sum(l.car_mask.astype(int) for l in self.layers)
+            crash_map = np.max(0, car_map-1)
+            return list(zip(*np.nonzero(crash_map))), crash_map.sum()
+
+        # check moveability
+        for n, l in enumerate(layers):
+            for pos in l.car_pos_iter():
+                if can_move(pos, n, layers, tl):
+                    l.mark_car(pos)
+        # move cars
+        goal_cars = 0
+        for l in self.layers:
+            goal_cars += l.advance_cars()
+        # check collisions
+        coll_pos, crashed_cars = collision_check(layers)
+        for l in self.layers:
+            l.remove_cars(coll_pos)
+        return goal_cars, crashed_cars
+
+    def plot(self, plt) -> List[Any]:
+        colors = ["tab:red", "tab:blue", "tab:cyan", "tab:orange"]
+        ret = [tl.plot(plt)]
+        for n, l in enumerate(self.layers):
+            ret.extend(l.plot(plt, offset=(n / len(self.layers) - 0.5) / 2, color=colors[n]))
+        return ret
+            
 
 artists = []
 artists.append(plot_layers(layers))
