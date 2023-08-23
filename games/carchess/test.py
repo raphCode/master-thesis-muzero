87e25495ca4ce4614fd97d64ceba6ef16a8f6cf2
raphTec <git@raphtec.net>
2023-08-23 15:50:53 +0200

    Fix typing, fix errors, reformat code
    Plotting and simulation works with the Map class, and I have not even
    stepped through the code with the debugger. What a pleasant surprise!

--- games/carchess/test.py
+++ games/carchess/test.py
@@ -1,19 +1,24 @@
+from __future__ import annotations
+
+import operator
+import itertools
 from os import path
 from enum import IntEnum
 from typing import *
-from functools import cache
-from collections.abc import *
+from functools import cache, cached_property
+from contextlib import suppress
+from collections.abc import Iterable, Iterator
 
 import numpy as np
 import imageio.v3 as iio
 import numpy.typing as npt
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
+import matplotlib.pyplot as plt  # type: ignore [import]
+import matplotlib.animation as animation  # type: ignore [import]
 
 Pos: TypeAlias = tuple[int, int]
 ndarr_bool: TypeAlias = npt.NDArray[np.bool_]
-ndarr_int: TypeAlias = npt.NDArray[np.int]
+ndarr_int: TypeAlias = npt.NDArray[np.int64]
+ndarr_f64: TypeAlias = npt.NDArray[np.float64]
 
 rng = np.random.default_rng()
 
@@ -24,26 +29,25 @@ class Dir(IntEnum):
     South = 2
     West = 3
 
-    def turn(self, offset: int) -> Self:
+    def turn(self, offset: int) -> Dir:
         return Dir((self + offset) % len(Dir))
 
     @property
-    def counter_clockwise(self) -> Self:
+    def counter_clockwise(self) -> Dir:
         return self.turn(-1)
 
     @property
-    def clockwise(self) -> Self:
+    def clockwise(self) -> Dir:
         return self.turn(1)
 
     def offset(self, pos: Pos) -> Pos:
-        pos = np.array(pos)
         offset = {
             Dir.North: (-1, 0),
             Dir.East: (0, 1),
             Dir.South: (1, 0),
             Dir.West: (0, -1),
         }[self]
-        return tuple(pos + offset)
+        return tuple(np.array(pos) + offset)  # type: ignore [return-value]
 
 
 class TrafficLights:
@@ -54,10 +58,10 @@ class TrafficLights:
         self.lights = mask
         self.reset()
 
-    def reset(self, percent_closed:float=0.5):
+    def reset(self, percent_closed: float = 0.5):
         self.closed = self.lights & (rng.random(self.lights.shape) < percent_closed)
 
-    def toggle_at(self,pos)->None:
+    def toggle_at(self, pos) -> None:
         assert self.lights[pos]
         self.closed[pos] ^= True
 
@@ -78,9 +82,9 @@ class Layer:
     pos: list[Pos]
     cars: set[int]  # indices of cars along the path
     moveable_cars: set[int]
-    spawn_counter:int
+    spawn_counter: int
 
-    def __init__(self, mask: ndarr_bool):
+    def __init__(self, mask: ndarr_int):
         def highspot_to_start_info(x, y) -> tuple[Pos, Dir]:
             mx, my = mask.shape
             if x == 0:
@@ -97,7 +101,7 @@ class Layer:
                 return (x + 1, y), Dir.West
             assert False, f"Invalid highest spot: ({x}, {y})"
 
-        self.size = tuple(mask.shape)
+        self.size = tuple(mask.shape)  # type: ignore [assignment]
         highspot_pos = np.unravel_index(np.argmax(mask), mask.shape)
         pos, dir = highspot_to_start_info(*highspot_pos)
 
@@ -121,41 +125,51 @@ class Layer:
                 ), f"Did not find next field! pos: ({pos[0]}, {pos[1]}), dir: {dir.name}"
         self.reset()
 
-    def reset(self, prepopulate: float=0.5)->None:
+    def reset(self, prepopulate: float = 0.5) -> None:
         n = len(self.pos) - 1
         self.cars = set((np.arange(n)[rng.random(n) < prepopulate]))
         self.moveable_cars = set()
-    def update_spawn_count(self,min_spawn:int, max_spawn:int, max_density:float)->None:
-        max_capacity = len(self.pos) -2 # minus first and last field
-        self.spawn_counter=min(int(max_capacity*max_density),self.spawn_counter+ rng.integers(min_spawn, max_spawn, endpoint=True))
+
+    def update_spawn_count(
+        self, min_spawn: int, max_spawn: int, max_density: float
+    ) -> None:
+        max_capacity = len(self.pos) - 2  # minus first and last field
+        self.spawn_counter = min(
+            int(max_capacity * max_density),
+            self.spawn_counter + rng.integers(min_spawn, max_spawn, endpoint=True),
+        )
 
     @cache
-    def offset_pos(self, pos, offset: int = 1) -> Pos:
-        return self.pos[self.index_at(pos) + offset]
+    def offset_pos(self, pos: Pos, offset: int = 1) -> Pos:
+        index = self.index_at(pos)
+        assert index is not None
+        return self.pos[index + offset]
 
     @cache
-    def index_at(self, pos) -> Optional[int]:
-        try:
+    def index_at(self, pos: Pos) -> Optional[int]:
+        with suppress(ValueError):
             return self.pos.index(pos)
-        except ValueError:
-            return None
+        return None
 
     @cache
-    def dir_at(self, pos) -> Dir:
+    def dir_at(self, pos: Pos) -> Dir:
         for d in Dir:
             if d.offset(pos) == self.offset_pos(pos, 1):
                 return d
+        raise ValueError
 
-    def is_car(self, pos) -> bool:
+    def is_car(self, pos: Pos) -> bool:
         return self.index_at(pos) in self.cars
 
-    def mark_car(self, pos) -> None:
-        self.moveable_cars.add(self.index_at(pos))
+    def mark_car(self, pos: Pos) -> None:
+        index = self.index_at(pos)
+        assert index is not None
+        self.moveable_cars.add(index)
 
     def advance_cars(self) -> bool:
         self.cars -= self.moveable_cars
         self.cars.update(x + 1 for x in self.moveable_cars)
-        goal_index =len(self.pos)-1
+        goal_index = len(self.pos) - 1
         reached_goal = goal_index in self.cars
         self.cars.discard(goal_index)
         self.moveable_cars = set()
@@ -164,40 +178,41 @@ class Layer:
     def remove_cars(self, pos: Iterable[Pos]) -> None:
         self.cars.difference_update(map(self.index_at, pos))
 
+    @property
     def car_pos_iter(self) -> Iterator[Pos]:
         # TODO: make faster
-        getter=operator.itemgetter(*self.cars)
-        ret=getter(self.pos)
+        getter = operator.itemgetter(*self.cars)
+        ret = getter(self.pos)
         yield from (self.pos[i] for i in self.cars)
 
     @property
     def lane_mask(self) -> ndarr_bool:
         ret = np.zeros(self.size, dtype=bool)
-        x,y=zip(*self.pos)
-        ret[x,y] = True
+        x, y = zip(*self.pos)
+        ret[x, y] = True
         return ret
 
     @property
     def car_mask(self) -> ndarr_bool:
         ret = np.zeros(self.size, dtype=bool)
-        x,y =zip(*self.car_pos_iter)
-        ret[x,y] = True
+        x, y = zip(*self.car_pos_iter)
+        ret[x, y] = True
         for index in self.cars:
             ret[self.pos[index]] = True
         return ret
 
     @property
     def observation_maps(self) -> tuple[ndarr_f64, ndarr_int, ndarr_int]:
-        x,y=zip(*self.pos)
+        x, y = zip(*self.pos)
         lane_map = np.full(self.size, -1)
-        lane_map[x,y] = np.linspace(1, 0, len(self.pos))
+        lane_map[x, y] = np.linspace(1, 0, len(self.pos))
         spawn_map = np.zeros(self.size, dtype=int)
-        spawn_map[self.pos[0]] =self.spawn_counter
+        spawn_map[self.pos[0]] = self.spawn_counter
         car_map = self.car_mask.astype(int) * 2 - 1
-        return lane_map,car_map, spawn_map
+        return lane_map, car_map, spawn_map
 
     def plot(self, plt, offset: float, **kwargs: Any) -> List[Any]:
-        def coords(pos:ndarr_f64):
+        def coords(pos: ndarr_f64):
             y, x = pos.T
             return x, -y
 
@@ -210,80 +225,82 @@ class Layer:
         return line_plots
 
 
-
-from contextlib import suppress
-
-import itertools
-
-
 class Map:
     """
     Map with layers, traffic lights and cars. Provides simulation interface and access to observation tensors.
     """
-    layers:list[Layer]
-    tl:TrafficLights
+
+    layers: list[Layer]
+    tl: TrafficLights
 
     def __init__(self, map_path: str):
         # rounds:int = 20,actions_per_round:int=2, reward_car:float=2, reward_crash :float=-1
-        def read_map_img(name:str)->ndarr_int:
-            return iio.imread(path.join(map_path,name))
-        def alpha_mask(img:ndarr_f64)->ndarr_bool:
+        def read_map_img(name: str) -> ndarr_int:
+            return iio.imread(path.join(map_path, name))
+
+        def alpha_mask(img: ndarr_int) -> ndarr_bool:
             return img[:, :, -1] > 0
-        self.tl=TrafficLights(alpha_mask(read_map_img( "tl.png") ))
+
+        self.tl = TrafficLights(alpha_mask(read_map_img("tl.png")))
         shape = self.tl.lights.shape
-        self.layers=[]
+        self.layers = []
         with suppress(FileNotFoundError):
             for layer_id in itertools.count(1):
-                img=read_map_img(f"{layer_id}.png")
-                assert img.shape[:2] == shape, f"Shape of layer {layer_id} map {img.shape[:2]} does not match the shape of the traffic light map {shape}"
+                img = read_map_img(f"{layer_id}.png")
+                assert (
+                    img.shape[:2] == shape
+                ), f"Shape of layer {layer_id} map {img.shape[:2]} does not match the shape of the traffic light map {shape}"
                 red_channel = img[:, :, 0]
                 l = Layer(red_channel * alpha_mask(img))
                 self.layers.append(l)
 
-    def reset(self, prepopulate: float=0.3,  init_red_rate:float=0.5)->None:
-        del self.car_observations # evict cache
+    def reset(self, prepopulate: float = 0.3, init_red_rate: float = 0.5) -> None:
         self.tl.reset(init_red_rate)
         for layer in self.layers:
             layer.reset(prepopulate)
-        while (coll_pos, _) := self.get_collisions:
-            random_layer = layers[rng.integers(len(layers))]
+        while True:
+            coll_pos, _ = self._get_collisions()
+            if not coll_pos:
+                break
+            random_layer = self.layers[rng.integers(len(self.layers))]
             random_layer.remove_cars(coll_pos)
 
-    def update_spawn_counts(min_spawn:int, max_spawn:int, max_density:float)->None:
-        # car_density:float=0.5, spawn_rate:tuple[int,int] 
+    def update_spawn_counts(
+        self, min_spawn: int, max_spawn: int, max_density: float
+    ) -> None:
         for l in self.layers:
             l.update_spawn_count(min_spawn, max_spawn, max_density)
 
-    @cached_property
-    def car_observations(self)->list[ndarr_f64]:
-        channels = []
+    def get_car_observations(self) -> list[ndarr_int | ndarr_f64]:
+        channels: list[ndarr_int | ndarr_f64] = []
         for l in self.layers:
             channels.extend(l.observation_maps)
         return channels
 
-    @property
-    def observation(self)->ndarr_f64:
-        return np.stack([self.tl.observation_map] + self.car_observations)
+    def get_observation(
+        self, car_observations: Optional[list[ndarr_int | ndarr_f64]] = None
+    ) -> ndarr_f64:
+        if car_observations is None:
+            car_observations = self.get_car_observations()
+        return np.stack([self.tl.observation_map] + car_observations)  # type: ignore [arg-type, operator]
 
     @cached_property
-    def observation_shape(self)->tuple[int, int,int]:
-        return (3*len(self.layers),* self.tl.lights.shape)
+    def observation_shape(self) -> tuple[int, int, int]:
+        return (1 + 3 * len(self.layers), *self.tl.lights.shape)  # type: ignore [return-value]
 
-    def toggle_tl(self,pos)->None:
+    def toggle_tl(self, pos) -> None:
         self.tl.toggle_at(pos)
 
-    def _get_collisions() -> tuple[list[Pos], int]:
+    def _get_collisions(self) -> tuple[list[Pos], int]:
         car_map = sum(l.car_mask.astype(int) for l in self.layers)
-        crash_map = np.max(0, car_map-1)
-        return list(zip(*np.nonzero(crash_map))), crash_map.sum()
+        crash_map = np.maximum(0, car_map - 1)
+        return list(zip(*np.nonzero(crash_map))), crash_map.sum()  # type: ignore [arg-type]
 
-    def simulation_step()->tuple[int,int]:
-        del self.car_observations # evict cache
-
+    def simulation_step(self) -> tuple[int, int]:
         # local function with local cache:
         # cache is correctly evicted after each simulation step
         @cache
-        def can_move(pos: Pos, layer_id: int)->bool:
+        def can_move(pos: Pos, layer_id: int) -> bool:
             """
             Compute whether a car can move to its next field.
             Implemented with dynamic programming and recursion.
@@ -305,9 +322,9 @@ class Map:
             return True
 
         # check moveability
-        for n, l in enumerate(layers):
-            for pos in l.car_pos_iter():
-                if can_move(pos, n, layers, tl):
+        for n, l in enumerate(self.layers):
+            for pos in l.car_pos_iter:
+                if can_move(pos, n):
                     l.mark_car(pos)
         # move cars
         goal_cars = 0
@@ -319,7 +336,7 @@ class Map:
             l.remove_cars(coll_pos)
         return goal_cars, crashed_cars
 
-    def plot_grid(self, ax)->Any:
+    def plot_grid(self, ax) -> Any:
         ax.set_xticks(np.arange(100) + 0.5)  # , labels="")
         ax.set_yticks(-np.arange(100) + 0.5)  # , labels="")
         ax.grid(color="grey")
@@ -327,11 +344,14 @@ class Map:
 
     def plot(self, plt) -> List[Any]:
         colors = ["tab:red", "tab:blue", "tab:cyan", "tab:orange"]
-        ret = [tl.plot(plt)]
+        ret = [self.tl.plot(plt)]
         for n, l in enumerate(self.layers):
-            ret.extend(l.plot(plt, offset=(n / len(self.layers) - 0.5) / 2, color=colors[n]))
+            ret.extend(
+                l.plot(plt, offset=(n / len(self.layers) - 0.5) / 2, color=colors[n])
+            )
         return ret
-            
+
+
 m = Map("maps/map1")
 m.reset()
 
@@ -340,19 +360,10 @@ fig, ax = plt.subplots()
 grid = m.plot_grid(ax)
 
 artists = []
-artists.append(plot_layers())
 
 for step in range(20):
-    artists.append(plot_layers(layers))
-    for n, l in enumerate(layers):
-        for pos in l.car_pos_iter():
-            if can_move(pos, n, layers, tl):
-                l.mark_car(pos)
-    for l in layers:
-        l.advance_cars()
-    resolve_collisions(layers)
-    if step % 5 == 0:
-        tl.reset()
+    artists.append(m.plot(ax))
+    m.simulation_step()
 
 ani = animation.ArtistAnimation(fig=fig, artists=artists, interval=1000)
 
