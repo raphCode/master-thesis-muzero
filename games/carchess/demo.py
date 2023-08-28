eb4c7a3f9561c2bc281552b61c5e6347fef83781
raphTec <git@raphtec.net>
2023-08-28 13:30:23 +0200

    Split implementation into properly named files

---
+++ games/carchess/demo.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt  # type: ignore [import]
+import matplotlib.animation as animation  # type: ignore [import]
+
+from .map import Map
+
+m = Map("maps/map1")
+m.reset()
+
+fig, ax = plt.subplots()
+
+m.plot_grid(ax)
+
+artists = []
+
+for step in range(20):
+    artists.append(m.plot(ax))
+    m.simulation_step()
+
+ani = animation.ArtistAnimation(fig=fig, artists=artists, interval=1000)
+
+plt.show()
