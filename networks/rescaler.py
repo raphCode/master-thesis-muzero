93a9718d08e5cbf5c9bdc4dc85da53f6e6ddc2d2
raphTec <git@raphtec.net>
2023-09-21 13:47:17 +0200

    Fix jit support of Rescaler, denote shapes better

--- networks/rescaler.py
+++ networks/rescaler.py
@@ -116,11 +116,14 @@ class Rescaler(RescalerPy, nn.Module):
         return mse * loss  # type: ignore [no-any-return]
 
     def jit(self) -> RescalerJit:
+        B = 1
+        S = len(self.support)
+        V = 1
         traced_mod = torch.jit.trace_module(  # type: ignore [no-untyped-call]
             self,
             dict(
-                forward=torch.zeros(len(self.support), 1),
-                get_target=torch.zeros(1, 1),
+                forward=torch.zeros(B, S, V),
+                calculate_loss=(torch.zeros(B, S, V), torch.zeros(B, V)),
             ),
         )
         object.__setattr__(traced_mod, "__class__", RescalerJit)
