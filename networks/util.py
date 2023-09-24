bec9fecd16e2e977d462d08222aa04c3e27196b9
raphTec <git@raphtec.net>
2023-09-24 19:54:21 +0200

    Add Identity representation network

--- networks/util.py
+++ networks/util.py
@@ -1,9 +1,12 @@
 from typing import Any
 
+import torch
 import torch.nn.functional as F
 from torch import Tensor, nn
 
 from util import copy_type_signature
+
+from .bases import RepresentationNet
 
 
 class NecroReLu(nn.Module):
@@ -15,3 +18,14 @@ class NecroReLu(nn.Module):
     @copy_type_signature(forward)  # provide typed __call__ interface
     def __call__(self, *args: Any, **kwargs: Any) -> Any:
         return super().__call__(*args, **kwargs)
+
+
+class ID(RepresentationNet):
+    def __init__(self, *args: Any, **kwargs: Any):
+        super().__init__()
+
+    def forward(
+        self,
+        *observations: Tensor,
+    ) -> Tensor:
+        return torch.cat([o.flatten(1) for o in observations], dim=1)
