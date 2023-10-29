1ff780bd6aaf2c9e3ae0c427d178544e50170103
raphTec <git@raphtec.net>
2023-10-29 16:05:01 +0100

    Add tensorboard data download script

---
+++ experiment_data/download.sh
@@ -0,0 +1,20 @@
+tags=(
+  "loss/latent"
+  "loss/turn"
+  "loss/policy"
+  "loss/reward"
+  "loss/value"
+  "selfplay/score00"
+  "prediction error/reward mse"
+  "prediction error/value mse"
+)
+
+for run in "$@"
+do
+  echo "DOWNLOAD $run"
+  mkdir -p "$run"
+  for tag in "${tags[@]}"
+  do
+    curl "http://localhost:6009/data/plugin/scalars/scalars?tag=${tag// /+}&run=${run// /+}/tb&format=csv" > "$run/${tag//\// }.csv"
+  done
+done
