49781da6337d97f24eeb5e75742e870d480736fb
raphTec <git@raphtec.net>
2023-11-01 23:46:29 +0100

    Update carchess plots to rolling mean + std

--- experiment_data/plot_carchess.py
+++ experiment_data/plot_carchess.py
@@ -7,19 +7,21 @@ file = sys.argv[1]
 raw = polars.read_csv(file)
 steps = raw["Step"]
 
-w = 5
+w = 30
 
 v = raw["Value"]
 v_smoothed = v.ewm_mean(alpha=0.03, adjust=False)
-v_min = v.rolling_min(w, min_periods=0).ewm_mean(alpha=0.03, adjust=False)
-v_max = v.rolling_max(w, min_periods=0).ewm_mean(alpha=0.03, adjust=False)
+mean = v.rolling_mean(w, min_periods=0)
+std = v.rolling_std(w, min_periods=0)
+v_min = v_smoothed - std * 1.96
+v_max = v_smoothed + std * 1.96
 
 print("steps", steps[-1])
 
 fig, ax = plt.subplots()
 fig.set_size_inches(7, 4)
 
-ax.plot(steps, v_smoothed)
+ax.plot(steps, mean)
 ax.fill_between(steps, v_min, v_max, alpha=0.5)
 
 ax.set_xlabel("Environment steps")
